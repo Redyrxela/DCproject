e69069eb590369d2cf267a6746d13bfcdcfd7d9c
Redyrxela <redyrxela@hotmail.com>
2014-01-15 20:23:16 -0500

    Update main.java

--- src/main.java
+++ src/main.java
@@ -6,5 +6,8 @@
 //I am thinking we have this as the "client/server" as it will be easy to port to all platforms
 //the server/router we will have as a different class
 
+//feel free to add classes  with things you are working on, lets try and keep these main two files
+//with "working code" so they will compile
+
 public class main {
 }
