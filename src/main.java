6c737e526bbd1c32766d91d9e61e508f71a1d67b
Redyrxela <redyrxela@hotmail.com>
2014-01-15 20:19:57 -0500

    basic empty class files

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
 /**
  * Created by Alex on 1/15/14.
  */
+
+
+//I am thinking we have this as the "client/server" as it will be easy to port to all platforms
+//the server/router we will have as a different class
+
 public class main {
 }
